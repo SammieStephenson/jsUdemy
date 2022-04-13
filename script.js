eb50155a358ff51800164eb95ea86b49f29aa77e
Sammie Stephenson <sammiestephenson@MacBook-Pro.attlocal.net>
2022-04-13 16:43:33 -0400

    second time

---
+++ script.js
@@ -0,0 +1,55 @@
+// let (age) for Mutation
+
+// const (birthyear) no change 
+
+// Don't want a lot of changes
+
+// don't use var regularly not semantic 
+
+// use node filename.js in terminal to show console log value
+
+// var myName = "Sammie";
+// var myAge = 25;
+// var isRemote = true;
+
+// console.log("My name is " + myName);
+// console.log("My name is " + myName + " my age is " + myAge.toString());
+
+// console.log(1 + 2);
+// var x = 1;
+// var y = 2;
+// console.log(x.toString() + y.toString());
+
+
+// // operator in class work 4/13
+// // mod divide OR remainder divide 
+// let modNum = 11 % 2
+// console.log(`${modNum}`)
+// // `${modNum}` is a template literal saves time in strings
+
+// let shortjs = 4;
+// let numz = 10;
+// // price = price * discount
+// shortjs = shortjs + 5;
+// console.log(shortjs)
+
+// // comparison
+
+// let num = 5;
+// let num2 = '5';
+
+// console.log(num == num2);
+
+// num = 5;
+// num2 = 5;
+
+// console.log(typfeOf num)
+// console.log(num == num2);
+
+const favorite = prompt("What is your favorite number?");
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite == 23)
+    console.log('Cool! 23 is Fun!')
+
